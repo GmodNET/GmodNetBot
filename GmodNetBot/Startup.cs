d09d6468c05030d00eee966f145e48a242bf5e71
Gleb Krasilich <gleb.chili@gmail.com>
2020-12-06 18:54:49 +0300

    Page 404 added

--- GmodNetBot/Startup.cs
+++ GmodNetBot/Startup.cs
@@ -24,6 +24,7 @@ namespace GmodNetBot
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddRazorPages();
+            services.AddHttpContextAccessor();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
